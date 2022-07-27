9603fb38903998c5b0fadbcf9df10105785fb006
Leela Revathi <106118772+LeelaRevathi@users.noreply.github.com>
2022-07-26 23:31:15 -0400

    Update Fish.py

--- Fish.py
+++ Fish.py
@@ -2,10 +2,12 @@ import numpy as np
 import pickle
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
+import os
 
 app=Flask(__name__)
 pickle_in = open("Fish_LR.pkl","rb")
 rf = pickle.load(pickle_in)
+port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
 def home():
@@ -23,7 +25,7 @@ def predict():
     return render_template('index.html', prediction_text = 'The fish belongs to species {}'.format(str(prediction)))
 
 if __name__=='__main__':
-    app.run()
+    app.run(host='0.0.0.0',debug=True,port=port)
 
 
 
